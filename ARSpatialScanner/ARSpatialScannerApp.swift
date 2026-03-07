d2de44223d95df2ae9b51791e544d9d2d9a57e11
Sumesh sharma <sumeshsharma903@gmail.com>
2026-03-07 12:42:12 +0530

    Initial Commit

---
+++ ARSpatialScanner/ARSpatialScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARSpatialScannerApp.swift
+//  ARSpatialScanner
+//
+//  Created by Sumesh on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ARSpatialScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARSpatialScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARSpatialScanner
+//
+//  Created by Sumesh on 07/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
